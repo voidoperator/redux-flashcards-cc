03831c0dff05476e186edc1cc5d55b902beca2dd
voidoperator <julionunez@me.com>
2023-01-12 20:49:54 -0500

    feat: add new topics functionality to slice of state

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { addTopic } from "../features/topics/topicsSlice";
 import { v4 as uuidv4 } from "uuid";
-import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
+import ROUTES from "../app/routes";
 
 export default function NewTopicForm() {
   const [name, setName] = useState("");
   const [icon, setIcon] = useState("");
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name.length === 0) {
       return;
     }
-
-    // dispatch your add topic action here
+    dispatch(addTopic({ id: uuidv4(), name: name, icon }));
     history.push(ROUTES.topicsRoute());
   };
 
